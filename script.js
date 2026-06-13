827c5eaa0725e9f9371c927a4c04604e92577332
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-13 16:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 //your code here!
 const item=document.getElementById("infi-list");
-	let count=1
-function add(nums) {
-for (let i = 1; i < nums; i++) {
+for (let i = 1; i < 100; i++) {
 	const li=document.createElement("li")
 	li.innerText=`Item ${i}`
 	item.appendChild(li)
-   count++
 }
-}
-add(100)
 window.addEventListener("scroll", () => {
     if (
         window.innerHeight + window.scrollY >=
