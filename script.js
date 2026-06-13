25b34d4e406f79bc909edd695a72386316654f10
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-13 16:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
-
+const item=document.getElementById("infi-list");
+	let count=1
+function add(nums) {
+for (let i = 1; i < nums; i++) {
+	const li=document.createElement("li")
+	li.innerText=`Item ${i}`
+	item.appendChild(li)
+   count++
+}
+}
+add(100)
+window.addEventListener("scroll", () => {
+    if (
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight
+    ) {
+        addItems(2);
+    }
+});
